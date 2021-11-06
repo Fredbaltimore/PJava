09ea2976001b97f61f25aa526f0bf5bd0d1a8df4
Fredbaltimore <fredinmd410@gmail.com>
2021-11-05 20:35:29 -0400

    learning about file processing

--- src/JavaImtiaz/FileUtil.java
+++ src/JavaImtiaz/FileUtil.java
@@ -6,19 +6,26 @@ import java.util.Scanner;
 
 public class FileUtil {
     public static void main(String[] args) {
+        //takes user input asking for name and prints out greeting with the users name they just entered
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
 
         try {
             //reading random txt file
-            File file = new File("src/JavaImtiaz/random.txt");
-            Scanner scanner;
+            File file = new File("src/JavaImtiaz/random");
             //scanner to read the file
-            scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file);
             //while scanner has next line, meaning as long as lines of text
             //are in the file
             while (scanner.hasNextLine()){
+                //line of text will be stored in lines
                 String lines = scanner.nextLine();
+                //prints out each line
                 System.out.println(lines);
             }
+            //closing scanner when while loop is finished
             scanner.close();
 
         } catch (FileNotFoundException e) {
