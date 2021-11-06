3f46500c9d4095bd9eccae263badb8ca54b31f22
Fredbaltimore <fredinmd410@gmail.com>
2021-11-06 10:32:57 -0400

    learning about file processing

--- src/JavaImtiaz/FileS.java
+++ src/JavaImtiaz/FileS.java
@@ -8,7 +8,9 @@ public class FileS  {
     public static void main(String[] args) throws IOException {
         File f = new File("D:\\new-space\\PJava\\src\\JavaImtiaz\\sr.txt");
         FileWriter fw = new FileWriter(f);
-        fw.write("Added text to the file");
+//        fw.write("Added text to the file");
+        fw.append("appending more text \n");
+        fw.append("moreeeeeeee");
         fw.close();
     }
 }
