9e3a07b8b7b2e192c324c316594809085ba8492c
Fredbaltimore <fredinmd410@gmail.com>
2021-11-06 22:10:56 -0400

    adding some functionality to the "chatty" bot

--- src/JavaImtiaz/FileS.java
+++ src/JavaImtiaz/FileS.java
@@ -7,15 +7,21 @@ import java.util.Scanner;
 
 public class FileS  {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter text to go in the file ");
-        String t = sc.nextLine();
-        File f = new File("D:\\new-space\\PJava\\src\\JavaImtiaz\\sr.txt");
-        FileWriter fw = new FileWriter(f);
-//        fw.write("Added text to the file");
-        fw.append(t);
-        fw.append("\nappending more text\n");
-        fw.append("moreeeeeeee");
-        fw.close();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter text to go in the file ");
+//        String t = sc.nextLine();
+//        File f = new File("D:\\new-space\\PJava\\src\\JavaImtiaz\\sr.txt");
+//        FileWriter fw = new FileWriter(f);
+//
+//        fw.append(t);
+//        fw.append("\nappending more text\n");
+//        fw.append("moreeeeeeee");
+//        fw.close();
+        int mo = 41 % 3;
+        int moo = 41 % 5;
+        int m = 41 % 7;
+        System.out.println(mo);
+        System.out.println(moo);
+        System.out.println(m);
     }
 }
